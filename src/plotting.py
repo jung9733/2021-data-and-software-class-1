c988e60b8713f7b16fee43eaa3e2ef8fb752cd40
Hee Jun Cheong <cheongheejune@gmail.com>
2021-04-09 14:36:51 -0400

    cleanup warnings

--- src/plotting.py
+++ src/plotting.py
@@ -1,16 +1,19 @@
 #!/bin/python
 
+"""This is a simple plotting library."""
+
 # Import the libraries we are using. It is good practice to import all necessary
 # libraries in the first lines of a file.
+import os
+import sys
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-import os
-import sys
+
 
 # Create a function to read the data file
 def read_data(filename,delimiter=',',starting_row=0):
-    """This function reads data from a specified filename. 
+    """This function reads data from a specified filename.
     The specified filename should point to a .csv file."""
     # Create an array (a multi-dimensional table) out of our data file, full of text
     all_data = np.genfromtxt(filename, delimiter=delimiter,skip_header=5)
@@ -20,6 +23,9 @@ def read_data(filename,delimiter=',',starting_row=0):
     return temperature_data
 
 def process_data(temperature_data):
+    """Given some input temperature data this function converts
+    the second column from degrees Fahrenheit to Kelvin and appends
+    a new column with that data."""
     # Compute a new column by multiplying column number 1 to Kelvin
     temperature_kelvin = (temperature_data[:,1,None] - 32) * 5/9 + 273
 
@@ -28,20 +34,31 @@ def process_data(temperature_data):
     return processed_temperature_data
 
 def plot_data(processed_temperature_data, plot_filename):
+    """Given some input temperature data and a file name this fuction
+    plots the third column of the input data into a file with
+    the name plot_filename."""
     # Create a figure of the processed data
     temperature_figure = plt.figure()
-    temperature_plot = plt.bar (processed_temperature_data[:,0],processed_temperature_data[:,2], width=35, color='blue')
+    plt.bar (processed_temperature_data[:,0],
+             processed_temperature_data[:,2],
+             width=35,
+             color='blue')
 
     plt.show(block=True)
     temperature_figure.savefig(plot_filename)
 
 
 def convert_data(filename, output_filename):
+    """Read data from a scv file called filename into a
+    Pandas Dataframe, and write this
+    Dataframe into a json file called output_filename."""
     all_data = pd.read_csv(filename, index_col='Date', header=4)
     all_data.info()
     all_data.to_json(output_filename)
 
 def plot():
+    """Main program that reads a dataset, processes it,
+    plots it, and write the converted data into a json file."""
     input_file = "110-tavg-12-12-1950-2020.csv"
     plot_file = "temperature-over-time.pdf"
     json_output_file = "data_output.json"
@@ -52,7 +69,7 @@ def plot():
     input_filename = os.path.join(data_directory,input_file)
     plot_filename = os.path.join(results_directory,plot_file)
     json_filename = os.path.join(results_directory,json_output_file)
-    
+
     temperature_data = read_data(input_filename, starting_row=0)
     processed_temperature_data = process_data(temperature_data)
     plot_data(processed_temperature_data, plot_filename)
--- tests/test_plotting.py
+++ tests/test_plotting.py
@@ -1,4 +1,7 @@
-import sys, os
+"""This file contains all tests for our plotting library"""
+
+import sys
+import os
 import numpy as np
 import pandas as pd
 
@@ -9,16 +12,19 @@ sys.path.append(os.path.join(
 import src.plotting as plotting
 
 def test_plot():
-    assert(plotting.plot() == None)
+    """A test for plot() function."""
+    assert(plotting.plot() is None)
 
 def test_process_data():
+    """A test for process_data() function."""
     input_data = np.array([[0,32],[1,212]])
     function_output = plotting.process_data(input_data)
     expected_output = np.array([[0,32,273],[1,212,373]])
-    
+
     assert(np.all(function_output == expected_output))
 
 def test_read_data():
+    """A test for read_data() function."""
     input_file = "110-tavg-12-12-1950-2020.csv"
     data_directory = os.path.realpath(os.path.join(os.path.dirname(__file__),"..","data"))
     input_filename = os.path.join(data_directory,input_file)
@@ -28,6 +34,7 @@ def test_read_data():
     assert(temperature_data[0,1] == 51.39)
 
 def test_plot_data():
+    """A test for plot_data() function."""
     plot_file = "test_plot_data.pdf"
     results_directory = os.path.realpath(os.path.join(os.path.dirname(__file__),"..","results"))
     plot_filename = os.path.join(results_directory,plot_file)
@@ -36,12 +43,13 @@ def test_plot_data():
 
     if os.path.exists(plot_filename):
         os.remove(plot_filename)
-        
+
     plotting.plot_data(input_data, plot_filename)
 
     assert (os.path.exists(plot_filename))
 
 def test_convert_data():
+    """A test for convert_data() function."""
     input_file = "110-tavg-12-12-1950-2020.csv"
     json_output_file = "data_output.json"
 
@@ -56,4 +64,4 @@ def test_convert_data():
     input_data = pd.read_csv(input_filename, index_col='Date', header=4)
     output_data = pd.read_json(json_filename)
 
-    assert (True)
+    assert input_data.info() is output_data.info()
